d75268f7cc7918405b90e177e4c1a303577f5ef4
svkw <violetkim@hotmail.com>
2023-09-02 20:05:23 -0600

    test test test test test 5

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		System.out.println();
+		int x = 5, y = 4;
+		int sum = x + y;
+		System.out.println("sum is " + sum);
 
 	}
 
